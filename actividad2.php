49bc0d14a0b0461aea5c7199bf3ccb6348b318df
Diego260603 <yinga100@gmail.com>
2020-05-14 13:16:58 -0500

    FEAT: Agregado nombre y gustos

---
+++ actividad2.php
@@ -0,0 +1,17 @@
+<?php
+echo "<!DOCTYPE>";
+  echo "<body>
+          <section>
+            <div class='nombre'>
+              <h1><b><u>Me llamo</u></b></h1>
+              <span style='color= rgba(85,63,119,1)'>Diego Muriel González</span>
+              <hr>
+            </div>
+            <div class='gusto'>
+              <h1><b><u>Me Gusta</u></b></h1>
+              <span style='color= rgba(180,191,163,1)'>La música, los coches y aprender</span>
+            </div>
+          </section>
+        </body>
+  ";
+?>
