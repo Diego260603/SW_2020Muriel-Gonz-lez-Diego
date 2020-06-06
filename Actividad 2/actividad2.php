ea9194e5e7caf5c9a7626e84d596d82b4564e608
Diego260603 <65355590+Diego260603@users.noreply.github.com>
2020-06-05 20:24:06 -0500

    FEAT: Actividad 2

---
+++ Actividad 2/actividad2.php
@@ -0,0 +1,52 @@
+<?php
+echo '<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Actividad 2  |  D.M.G.</title>
+    <style media="screen">
+    body{
+      display: flex;
+      flex-flow: column wrap;
+      align-content: center;
+      background-color:rgba(126, 126, 126, 0.32);
+    }
+    p{
+      font-size: 40px;
+    }
+    .h1{
+      font-size: 80px;
+      text-decoration: overline underline;
+    }
+    .likelove{
+      width: 80%;
+      height: auto;
+    }
+    </style>
+  </head>
+  <body>
+  <div class="h1">
+    <h1><u>Diego Muriel González</u></h1>
+  </div>
+  <div class="likelove">
+    <p>He aprendido a dar estilo a mis páginas con CSS, además de hacer pensar a estas con PHP.</p>
+    <p>Me gusta la actitud de los profesores y la disposición de los mismos a responder dudas y ejemploficar los conceptos.</p>
+  </div>
+  </body>
+</html>';
+/*<style media="screen">
+body{
+  display: flex;
+  flex-flow: column wrap;
+  align-content: center;
+}
+  .h1{
+    font-size: 50vh;
+    text-decoration: overline underline;
+  }
+  @font-face{
+    font-family: "oswald";
+    src: url(../LIBRERIAS/fontawesome-free-5.13.0-web/FONTS/Oswald-VariableFont_wght.ttf);
+  }
+</style>*/
+?>
